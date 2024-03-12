1873d018e0f761b1d51bc74a8e516a7927364fd9
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-12 19:11:27 +0000

    Alphabet reversed, alternating lowercase and uppercase

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+tebahpla = []
+for i in reversed(range(97, 123)):
+    if i % 2 == 0:
+        tebahpla.append(chr(i))
+    else:
+        tebahpla.append(chr(i - 32))
+print("{}".format("".join(tebahpla)), end="")
