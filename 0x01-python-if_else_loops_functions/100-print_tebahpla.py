8ecbcb78fcbd71dce91c5e7da9eb1173e243a7e9
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-13 10:19:21 +0000

    Print Alphabet in reversed order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-tebahpla = []
 for i in reversed(range(97, 123)):
-    if i % 2 == 0:
-        tebahpla.append(chr(i))
-    else:
-        tebahpla.append(chr(i - 32))
-print("{}".format("".join(tebahpla)), end="")
+    print("{}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
