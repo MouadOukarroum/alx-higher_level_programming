fa70271d5990c5e4d9191123ea705b3ff493e30d
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-13 09:49:26 +0000

    Remove char at index n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     str_cp = []
-    for i in len(str):
+    for i in range(len(str)):
         if i != n:
             str_cp.append(str[i])
         else:
