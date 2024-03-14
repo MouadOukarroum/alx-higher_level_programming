bc93d3d868713a35a9155eb34cf2ed9f70de319e
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-14 21:39:47 +0000

    Dir() method v2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ import importlib
 
 def main():
     hidden_4 = importlib.import_module("hidden_4")
-    all_names = dir(hidden_4)
+    all_names = sorted(dir(hidden_4))
     for name in all_names:
         if not name.startswith("__"):
             print("{}".format(name))
