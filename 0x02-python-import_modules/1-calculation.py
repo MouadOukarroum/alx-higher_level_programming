0bb67dce0bee60fac05570da37e48c152f4b3da8
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-14 18:13:22 +0000

    Calculation, Importing a module from a fiel v4

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ from calculation_1 import sub, add, mul, div
 def main():
     a = 10
     b = 5
-    print(a, "+" ,b, "=", add(a, b))
+    print(a, "+", b, "=", add(a, b))
     print(a, "-", b, "=", sub(a, b))
     print(a, "*", b, "=", mul(a, b))
     print(a, "/", b, "=", div(a, b))
