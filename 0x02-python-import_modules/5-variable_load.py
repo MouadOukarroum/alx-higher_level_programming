2f0d04c3e50194240c7664f3cf579f04dec200f3
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-14 22:14:22 +0000

    Importing variables

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import variable_load_5
+import variable_load_5 as v
 
 
 def main():
-    print(variable_load_5.a)
+    var = v.a
+    print(var)
 
 
 if __name__ == "__main__":
