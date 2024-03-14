e90d4934efa85d3713270b3167a20cffb8fc95c5
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-14 19:21:56 +0000

    Comman Line Args v2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import sys
+
+
 def main():
-	args = sys.argv
-	length = len(args)
-	if length < 2:
-		print("{} argument.".format(length - 1))
-	else:
-	    print("{} arguments:".format(length - 1))
-	    for i in range(length):
-	        if i > 0:
-	            print("{}: {}".format(i, args[i]))
+    args = sys.argv
+    length = len(args)
+    if length < 2:
+        print("{} argument.".format(length - 1))
+    else:
+        print("{} arguments:".format(length - 1))
+        for i in range(length):
+            if i > 0:
+                print("{}: {}".format(i, args[i]))
+
 
 if __name__ == "__main__":
-	main()
+    main()
