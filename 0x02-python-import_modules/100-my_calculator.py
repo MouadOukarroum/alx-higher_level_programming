9d52c9b918fc0ad024c9df6938b7be27342b44c6
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-15 00:06:30 +0000

    Calculator v3

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,29 +2,24 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-
+    
+    print(len(sys.argv))
     args = sys.argv
     length = len(args)
-    operators = "+-*/"
     if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    elif not (args[2] in operators):
-        print("Unknown operator. Availabe operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
+    a = int(args[1])
+    op = args[2]
+    b = int(args[3])
+    if op == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif op == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        try:
-            a = int(args[1])
-            op = args[2]
-            b = int(args[3])
-            if op == "+":
-                print("{} + {} = {}".format(a, b, add(a, b)))
-            elif op == "-":
-                print("{} - {} = {}".format(a, b, sub(a, b)))
-            elif op == "*":
-                print("{} * {} = {}".format(a, b, mul(a, b)))
-            else:
-                print("{} / {} = {}".format(a, b, div(a, b)))
-        except ValueError:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-            sys.exit(1)
+        print("Unknown operator. Available operators: +, -, *, /")
+        exit(1)
