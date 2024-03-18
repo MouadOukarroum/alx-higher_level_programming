134ae858991a422acbb40fdf908d75d99ecc2a1d
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-18 18:20:54 +0000

    Lists in C

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * palindrome -recursive palind or not
+ * is_palindrome - recursive palind or not
  * @head: head of the list
  * Return: 0 if not a palind, otherwise 1
  */
@@ -10,22 +10,24 @@ int is_palindrome(listint_t **head)
 {
 	if (head == NULL || *head == NULL)
 		return (1);
-	return (aux_palind(head, *head);
+	return (aux_palind(head, *head));
 }
 
 /**
  * aux_palind - funct to check if a list a plaindrome
  * @head: head list
  * @end: list end
+ * Return: 0 if not a plaind, otherwise 1
  */
 
 int aux_palind(listint_t **head, listint_t *end)
 {
 	if (end == NULL)
 		return (1);
-	if (aux_palind(head, end->next) && (*head)->n == end ->n)
+	if (aux_palind(head, end->next) && (*head)->n == end->n)
 	{
 		*head = (*head)->next;
 		return (1);
 	}
+	return (0);
 }
