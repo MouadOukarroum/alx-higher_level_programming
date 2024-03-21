223ae3e72ccf2d0a8b29821955829892e9f7a935
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-21 01:18:03 +0000

    Uinuqe indeed

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
-#!/usr/bin/pythn3
+#!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique = set(my_list)
     result = 0
-    for i in unique:
-        result += i
+    for i in range(len(unique)):
+        result += unique[i]
     return result
