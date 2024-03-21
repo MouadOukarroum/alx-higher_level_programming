8a714f92e1c4ae1a4735ab38cf4dea08444f1bb6
MouadOukarroum <oukarroummouad@gmail.com>
2024-03-21 01:20:55 +0000

    Unique indeed v2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique = set(my_list)
     result = 0
-    for i in range(len(unique)):
-        result += unique[i]
+    if my_list:
+        unique = set(my_list)
+        for i in unique:
+            result += i
     return result
